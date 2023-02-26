9cb0ac6fb6fcbc304f18567966db6582794251ca
Dev <devill3boi@gmail.com>
2023-02-27 05:59:05 +0800

    initial commit of python web scraper along with json and csv datasources

---
+++ datasources/muis/get-full.py
@@ -0,0 +1,73 @@
+# import required modules 
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time 
+import json
+import re
+
+# create instance of Chrome webdriver
+options = Options()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options = options) 
+driver.get("https://www.muis.gov.sg/Halal/Halal-Certification/Certified-Eating-Establishments#") 
+
+driver.implicitly_wait(10)
+
+# enter asterisk 
+print('Searching with wildcard')
+driver.find_element("xpath",'//*[@id="txtHalalSearch"]').send_keys('*') 
+
+# click on Search 
+print('Clicking search')
+driver.find_element("xpath",
+	'//*[@id="btnHalalSearch"]').click() 
+time.sleep(8)
+
+# Initialize array to store restaurant data
+res = []
+
+# Iterate through 404 pages to scrape the data
+i=1
+rest_no = 0
+while i <= 403:
+    print('Scraping Page: ' + str(i))
+
+    # get all divs
+    results = driver.find_element("xpath",'//*[@id="results"]')
+
+    # parse results with soup
+    soup = BeautifulSoup(results.get_attribute('innerHTML'), 'html.parser')
+    time.sleep(8)
+
+    fullPage = soup.find('div', class_='search-result')
+    # print(fullPage)
+    restaurantNames = fullPage.find_all('p',class_='strong')
+    restaurantLocations = fullPage.find_all('div',class_='location')
+
+    #Iterate through all the restaurants on the page
+    for index, restaurant in enumerate(restaurantNames):
+        #name of restaurant
+        try:
+            name = restaurantNames[index].text.strip()
+            addressRaw = ''.join(restaurantLocations[index].find('br').previous_sibling)
+            address = re.sub(r"\n", "", addressRaw).strip()
+            postalCodeRaw = ''.join(restaurantLocations[index].find('br').next_sibling)
+            postalCode = re.sub(r"\n", "", postalCodeRaw).strip()
+            data ={'id': str(rest_no),'name':name, 'address':address, 'postalCode':postalCode}
+            res.append(data)
+            rest_no+=1
+        except:
+            print("Unable to parse: /n" + restaurant)
+            print("Found on page:" + i)
+
+    # Click next page
+    driver.find_element("xpath","//a[@title='>> Next']").click()
+
+    # Sleep while waiting for page to load
+    time.sleep(8)
+    i+=1
+
+with open("halal-eateries-small.json", "a",  encoding='utf-8') as f:
+    json.dump(res, f, indent=8, ensure_ascii=False)
---
+++ datasources/onemap-full.py
@@ -0,0 +1,61 @@
+import requests
+import csv
+import json
+import time
+import pathlib
+
+#.csv file write headers
+csvheaders = ['ID','LATITUDE','LONGITUDE']
+
+with open('muis_coordinates.csv','a',encoding ='utf-8',newline='') as c:
+    writer=csv.writer(c)
+    writer.writerow(csvheaders)
+
+dataSource=[]
+
+#open halal-eateries.json and read from it
+with open ('muis\halal-eateries.json', 'r') as a:
+    dataSource = json.load(a)
+    print('Finished reading json')
+
+#set up the url, headers and parameters
+url = 'https://developers.onemap.sg/commonapi/search'
+
+headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
+#iterate through json and get lat/long and dump to csv
+for source in dataSource:
+    resData = []
+    # print(source)
+    id = source['id']
+    postalCode = source.get('postalCode')
+    #print(id)
+
+    params = {
+        'searchVal': postalCode,
+        'returnGeom': 'Y',
+        'getAddrDetails':'N'
+    }
+
+    #call onemap api and format to JSON
+    response = requests.request("GET", url,headers=headers,params=params)
+    resJson = response.json()
+    print(resJson)
+
+    #parse JSON response for lat/long and write to csv
+    latitude = resJson['results'][0]['LATITUDE']
+    longitude = resJson['results'][0]['LONGITUDE']
+    data=[id, latitude, longitude]
+    print(data)
+    resData.append(data)
+
+
+    with open('muis_coordinates-small.csv','a',encoding ='utf-8',newline='') as b:
+        writer=csv.writer(b)
+        writer.writerows(resData)
+
+#alert when csv is done writing
+print('CSV completed')
