ecb892ce52ea4d6027dcd4b98411453f485d7825
Dev <devill3boi@gmail.com>
2023-02-27 07:17:11 +0800

    parse csv to array

--- apicalls.js
+++ apicalls.js
@@ -1,6 +1,28 @@
-coordsCsv = 'https://raw.githubusercontent.com/RecursiveDev/tgc-proj1/main/datasources/muis_coordinates-small.csv'
+const coordsCsv = 'https://raw.githubusercontent.com/RecursiveDev/tgc-proj1/main/datasources/muis_coordinates-small.csv'
+
+async function fetchCoords(){
+    let response = (await axios.get(coordsCsv)).data;
+    console.log(response);
+    return response;
+}
 
 async function getCoords(){
-    response = (await axios.get(coordsCsv)).data
-    console.log(response)
+    let coordsStr = await fetchCoords()
+    // console.log(coordsStr)
+    let lines = coordsStr.split("\n");
+    console.log(lines)
+    let parsedCoords = [];
+    let headers = lines[0].split(",");
+    console.log(headers)
+
+    for (let i=1;i<lines.length;i++){
+        let obj = {};
+        let row = lines[i].split(",");
+
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = row[j];
+        }
+        parsedCoords.push(obj);
+    }
+    console.log(parsedCoords)
 }
