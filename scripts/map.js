84c214c0c5e76705f2a830d4920ad2b807a31b98
dev <devill3boi@gmail.com>
2023-02-28 19:04:05 +0800

    test retrieving map view bounds

--- scripts/map.js
+++ scripts/map.js
@@ -1,27 +1,36 @@
-function getIcon(iconUrl){
-    let halalIcon = L.icon({
-        iconUrl: iconUrl,
-        iconSize:     [48, 48], // size of the icon
-        iconAnchor:   [18, 18], // point of the icon which will correspond to marker's location
-        popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
-    });
-    return halalIcon
-}
-
-
 let center = [1.3306,103.8158]; // center latlong
+
+let map = L.map('map').setView(center, 12);
 
 // setup the tile layers
 let mapBounds = new L.LatLngBounds(
     new L.LatLng(1.1443, 103.596),
     new L.LatLng(1.4835, 104.1));
 
-ompDay = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
-})
+function initTiles(locations){
+    ompNight = L.tileLayer('https://maps-{s}.onemap.sg/v3/Night/{z}/{x}/{y}.png', {
+        detectRetina: true,
+        maxZoom: 19,
+        minZoom: 13,
+        zoom: 13,
+        maxBounds: mapBounds,
+        maxBoundsViscosity: 1,
+        layers: locations
+    }).addTo(map);
+}
 
-ompNight = L.tileLayer('https://maps-{s}.onemap.sg/v3/Night/{z}/{x}/{y}.png', {
-})
+//set the icon parameters
+function getIcon(iconUrl){
+    let halalIcon = L.icon({
+        iconUrl: iconUrl,
+        iconSize:     [48, 48], 
+        iconAnchor:   [18, 18], 
+        popupAnchor:  [0, -10] 
+    });
+    return halalIcon
+}
 
+//populate layer with markers using foursquare icons
 async function populateMarkers(){
     let features = await getFeatures();
     let markers = [];
@@ -33,7 +42,7 @@ async function populateMarkers(){
         let iconUrl = 'https://ss3.4sqi.net/img/categories_v2/food/default_64.png'
         try{
         iconUrl = String(foursquare.results[0].categories[0].icon.prefix) + '64.png'
-        console.log(iconUrl);
+        // console.log(iconUrl);
         }
         catch{}
         let address = String(feature['address']);
@@ -53,33 +62,18 @@ async function buildMap(){
     markers = await populateMarkers()
     
     let locations = L.layerGroup(markers)
-
-    let map = L.map('map',{
-        detectRetina: true,
-        maxZoom: 19,
-        minZoom: 13,
-        center: center,
-        zoom: 13,
-        maxBounds: mapBounds,
-        maxBoundsViscosity: 1,
-        layers: ompDay, ompNight, locations
-
-    });
+    initTiles(locations);
 
     let baseLayers = {
-        "OneMap - Day Theme": ompDay,
-        "OneMap - Night Theme": ompNight.addTo(map)
-
-
     }
-    
+
     let overlayLayers = {
         "Halal Restaurants": locations.addTo(map)
     }
-
     let layerControl = L.control.layers(baseLayers, overlayLayers).addTo(map);
 }
 
 async function mapInit(){
    await buildMap();
+
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-mapInit();
+let testbtn = document.querySelector('#testbtn');
+testbtn.addEventListener('click', getFeaturesInView);
+
+function getFeaturesInView() {
+    console.log("button clicked");
+    var features = [];
+    map.eachLayer( function(layer) {
+      if(layer instanceof L.Marker) {
+        if(map.getBounds().contains(layer.getLatLng())) {
+          features.push(layer.getLatLng());
+        }
+      }
+    });
+    console.log(features);
+  }
+  mapInit();
